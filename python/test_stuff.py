71d0a89f184892703e859a5f59287beafc28b0f2
Murtaza Dhuliawala <Murtaza Dhuliawala>
2016-08-15 04:53:42 -0400

    Test

--- python/test_stuff.py
+++ python/test_stuff.py
@@ -1,4 +1,10 @@
 __author__ = 'Murtaza'
 
 def bitwise():
-    print bin(1<<3)
+    # To declare a binary number - put 0b in front of the number
+    print 0b011<<3
+    print int('11',2)
+    print int('11',2)<<3
+    print bin(0b11<<3)
+
+bitwise()

4a0556c622ccad72a33bd47199f903c808dcdbdd
Murtaza <murtzdhulz@gatech.edu>
2016-09-19 17:59:16 -0400

    Powerset

---
+++ python/PowerSet.py
@@ -0,0 +1,28 @@
+class PowerSet:
+    def powerSet1(self,set):
+        result = [[]]
+        for x in set:
+            # print 'For x=',x,[subset + [x] for subset in result]
+            result.extend([subset + [x] for subset in result])
+        return result
+
+    def powerSet2(self,set):
+        # find 2^n
+        result=[]
+        k=1<<len(set)
+        for i in range(k):
+            cur_set=self.intToSet(set,i)
+            result.append(cur_set)
+        return result
+
+    def intToSet(self,set,k):
+        cur_set=[]
+        for i in range(len(set)):
+            if k & 1<<i:
+                cur_set.append(set[i])
+        return cur_set
+
+
+ans = PowerSet()
+print ans.powerSet2([1,2,3])
+

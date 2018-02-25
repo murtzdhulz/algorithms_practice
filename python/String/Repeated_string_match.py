25e1ae93b603b0dec903e94bc985df1e3743976d
mdhuliawala3 <murtzdhulz@gmail.com>
2018-02-25 17:43:46 -0500

    Repeated string match

---
+++ python/String/Repeated_string_match.py
@@ -0,0 +1,28 @@
+__author__ = 'Murtaza'
+
+class Solution(object):
+    def repeatedStringMatch(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: int
+        """
+        times = -(-len(B)//len(A)) # 7/3
+        if B in A*times:
+            return times
+        elif B in A*(times+1):
+            return times+1
+        else:
+            return -1
+
+"""
+Question:
+Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it. If no such solution, return -1.
+
+For example, with A = "abcd" and B = "cdabcdab".
+
+Return 3, because by repeating A three times ("abcdabcdabcd"), B is a substring of it; and B is not a substring of A repeated two times ("abcdabcd").
+
+Note:
+The length of A and B will be between 1 and 10000.
+"""

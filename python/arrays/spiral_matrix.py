2873ce48966929710b122bbfece5de1c1b458cf9
Murtaza Dhuliawala <Murtaza@Murtazas-MacBook-Pro.local>
2018-03-06 01:10:58 -0500

    Spiral

---
+++ python/arrays/spiral_matrix.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def spiralOrderSimulation(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        # This is the simulation solution
+        if not matrix: return []
+        ans= []
+        dr = [0, 1, 0, -1]
+        dc = [1, 0, -1, 0]
+        R, C = len(matrix), len(matrix[0])
+        seen = [[False]*len(matrix[0]) for _ in matrix]
+        r=c=di=0
+        for _ in xrange(R*C):
+            ans.append(matrix[r][c])
+            seen[r][c]=True
+            cr, cc = r+dr[di], c+dc[di]
+            if 0<=cr<R and 0<=cc<C and not seen[cr][cc]:
+                r,c=cr,cc
+            else:
+                di=(di+1)%4
+                r,c = r+dr[di], c+dc[di]
+        return ans
+
+"""
+Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+For example,
+Given the following matrix:
+
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+You should return [1,2,3,6,9,8,7,4,5].
+"""
